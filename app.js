bc9d5de9ccb0c19c443a182098fa2d7ba699b91f
Benjamin Reed <rangerrick@befunk.com>
2012-10-03 00:22:05 -0400

    sync with webapp

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log("app.js loading");
 
+var eventsModel;
+
+/** filter dates in Knockout data-bind **/
 ko.bindingHandlers.dateString = {
 	update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
 		var value = valueAccessor(),
@@ -10,32 +13,21 @@ ko.bindingHandlers.dateString = {
 	}
 }
 
-function removeLastMatch(string, match) {
-	var n = string.lastIndexOf(match);
-	if (n >= 0 && (n + match.length) == string.length) {
-		// console.log("removeLastMatch: " + string + " matches " + match);
-		return string.substring(0, n);
-	} else {
-		// console.log("removeLastMatch: " + string + " does not match " + match);
-		return string;
-	}
-}
-
+/** represents a calendar event **/
 function Event(data) {
 	var self = this;
 
-	data.summary = removeLastMatch(data.summary, ' - ' + data.location);
-
-	self.id          = data.id;
-	self.summary     = ko.observable(data.summary);
-	self.description = ko.observable(data.description);
-	self.start       = ko.observable(new Date(data.startDate));
-	self.end         = ko.observable(new Date(data.endDate));
-	self.location    = ko.observable(data.location);
-	self.createdBy   = ko.observable(data.createdBy);
-	self.timespan    = ko.computed(function() {
+	self.id           = ko.observable(data["@id"]);
+	self.summary      = ko.observable(data.summary);
+	self.description  = ko.observable(data.description);
+	self.start        = ko.observable(new Date(data.start));
+	self.end          = ko.observable(new Date(data.end));
+	self.location     = ko.observable(data.location);
+	self.createdBy    = ko.observable(data["created-by"]);
+	self.owner        = ko.observable(data.owner);
+	self.timespan     = ko.computed(function() {
 		var start = start === null? null : formatTime(self.start(), false);
-		var end   = end   === null? null : formatTime(self.end(), false);
+		var end	= end	=== null? null : formatTime(self.end(), false);
 		var retVal = "";
 		if (start != null) {
 			retVal += start;
@@ -47,65 +39,152 @@ function Event(data) {
 	}, self);
 }
 
-var entryIdMatch = function(entry, id) {
-	return entry.id === id;
+/** used for filter/searching, match an event based on a filter **/
+var matchEventText = function(event, filter) {
+	if (event.summary().toLowerCase().search(filter) != -1) {
+		return true;
+	} else if (event.description().toLowerCase().search(filter) != -1) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
+/** Wait to make sure no other changes have happened,
+  * then perform the mildly expensive check to see what
+  * the latest visible element is.
+  **/
+var onFilterChange = function() {
+	/*
+	if (scrollTimeout === null) {
+		scrollTimeout = setTimeout(function() {
+			scrollTimeout = null;
+			var found = findTopVisibleElement();
+			if (found) {
+				console.log("visible element: " + $(found).find('div.summary').text() + ' (' + $(found).attr('id') + ')');
+			} else {
+				console.log("no elements visible!");
+			}
+		}, scrollEndDelay);
+	}
+	*/
+};
 
 function EventsViewModel() {
 	var self = this;
 	self.events = ko.observableArray();
-	self.filter = ko.observable("");
 
-	self.updateDataFromJSON = function() {
-	    $.getJSON("http://sin.local:8088/events.json", function(allData) {
-	        var mappedTasks = $.map(allData.eventList, function(event) {
-	        	var item = ko.utils.arrayFirst(self.events(), function(entry) {
-	        		if (entry.id == event.id) {
-	        			// console.log("found match: " + ko.toJSON(entry));
-	        			return true;
-	        		} else {
-	        			return false;
-	        		}
-	        	});
-	        	if (item) {
-//	        		console.log("reusing " + ko.toJSON(item));
-	        		var startDate = new Date(event.startDate);
-	        		var endDate   = new Date(event.endDate);
+	self.updateData = function(allData) {
+		var mappedTasks = $.map(allData.event, function(event) {
+			var item = ko.utils.arrayFirst(self.events(), function(entry) {
+				if (entry) {
+					// console.log("entry = " + ko.toJSON(entry));
+					if (entry.id() == event["@id"]) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					console.log("no entry");
+				}
+			});
+			if (item) {
+				// console.log("reusing " + ko.toJSON(item));
+				var startDate = new Date(event.start);
+				var endDate	= new Date(event.end);
+				var createdBy = event["created-by"];
 
-	        		if (item.summary()         != event.summary)       { item.summary(event.summary); }
-	        		if (item.description()     != event.description)   { item.description(event.description); }
-	        		if (item.start().getTime() != startDate.getTime()) { item.start(startDate); }
-	        		if (item.end().getTime()   != endDate.getTime())   { item.end(endDate); }
-	        		if (item.createdBy         != event.createdBy)     { item.createdBy(event.createdBy); }
-	        		return item;
-	        	} else {
-		        	return new Event(event);
-	        	}
-	        });
-	        self.events(mappedTasks);
-	    });    
-	};
+				if (item.summary()         != event.summary)       { item.summary(event.summary); }
+				if (item.description()     != event.description)   { item.description(event.description); }
+				if (item.start().getTime() != startDate.getTime()) { item.start(startDate); }
+				if (item.end().getTime()   != endDate.getTime())   { item.end(endDate); }
+				if (item.createdBy()       != createdBy)           { item.createdBy(createdBy); }
+				if (item.owner()           != event.owner)         { item.owner(event.owner); }
+				return item;
+			} else {
+				return new Event(event);
+			}
+		});
+		self.events(mappedTasks);
+		console.log("saving ReST events");
+		amplify.store("events", allData);
+	}
 	
+	self.updateDataFromJSON = function() {
+		$.getJSON("rest/events", self.updateData);
+	};
+
 	self.updateDataFromJSON();
 }
 
-var eventsModel = new EventsViewModel();
-eventsModel.filteredEvents = ko.dependentObservable(function() {
-	var filter = this.filter().toLowerCase();
-	
-	if (!filter) {
-		return this.events();
+eventsModel = new EventsViewModel();
+
+if (typeof(Storage) !== "undefined") {
+	var restEvents = amplify.store("events");
+	if (restEvents) {
+		console.log("loading stored ReST events");
+		eventsModel.updateData(restEvents);
 	} else {
-		return ko.utils.arrayFilter(this.events(), function(event) {
-			if (event.summary().toLowerCase().search(filter) != -1) {
-				return true;
-			} else if (event.description().toLowerCase().search(filter) != -1) {
-				return true;
-			} else {
-				return false;
-			}
+		console.log("no stored ReST events");
+	}
+}
+
+function OfficialEventsModel() {
+	var self   = this;
+
+	self.filter = ko.observable("");
+	self.events = eventsModel.events;
+}
+var officialEventsModel = new OfficialEventsModel();
+officialEventsModel.filter.subscribe(onFilterChange, officialEventsModel);
+officialEventsModel.filteredEvents = ko.dependentObservable(function() {
+	var self = this;
+
+	var filter = self.filter().toLowerCase();
+
+	var matchesGroup = ko.utils.arrayFilter(self.events(), function(event) {
+		if (event.owner() != 'admin') {
+			return false;
+		}
+		return true;
+	});
+
+	if (!filter) {
+		return matchesGroup;
+	} else {
+		return ko.utils.arrayFilter(matchesGroup, function(event) {
+			return matchEventText(event, filter);
 		});
 	}
-}, eventsModel);
+}, officialEventsModel);
+
+function MyEventsModel() {
+	var self   = this;
+
+	self.filter = ko.observable("");
+	self.events = eventsModel.events;
+}
+var myEventsModel = new MyEventsModel();
+myEventsModel.filter.subscribe(onFilterChange, myEventsModel);
+myEventsModel.filteredEvents = ko.dependentObservable(function() {
+	var self = this;
+
+	var filter = self.filter().toLowerCase();
+
+	var matchesGroup = ko.utils.arrayFilter(self.events(), function(event) {
+		if (event.owner() == 'admin') {
+			return false;
+		}
+		return true;
+	});
+
+	if (!filter) {
+		return matchesGroup;
+	} else {
+		return ko.utils.arrayFilter(matchesGroup, function(event) {
+			return matchEventText(event, filter);
+		});
+	}
+}, myEventsModel);
 
 console.log("app.js loaded");
--- init.js
+++ init.js
@@ -1,11 +1,109 @@
 console.log("init.js loading");
 
+var pages = {};
+var current_page = 'official-events';
+var page_scroll_element = [];
+
 var templates = {
 	header: "views/header.html",
 	events: "views/events.html",
 	loaded: 0,
 	requested: 0,
 };
+
+function elementInViewport(el) {
+	var top = el.offsetTop;
+	// var left = el.offsetLeft;
+	// var width = el.offsetWidth;
+	var height = el.offsetHeight;
+
+	while (el.offsetParent) {
+		el = el.offsetParent;
+		top += el.offsetTop;
+		// left += el.offsetLeft;
+	}
+
+	return (top >= window.pageYOffset
+		// && left >= window.pageXOffset
+		&& (top + height) <= (window.pageYOffset + window.innerHeight)
+		// && (left + width) <= (window.pageXOffset + window.innerWidth)
+	);
+}
+
+function _getPageStoreCache() {
+	// console.log("store currently contains: " + ko.toJSON(amplify.store()));
+	var page_store_cache = amplify.store('page_store_cache');
+	if (!page_store_cache) {
+		page_store_cache = {};
+	}
+	return page_store_cache;
+}
+
+function _setPageStoreCache(cache) {
+	console.log("set cache: " + ko.toJSON(cache));
+	amplify.store('page_store_cache', cache);
+}
+
+function updatePageTopElement(page, id) {
+	console.log("storing " + id + " as the top element for page " + page);
+	var page_store_cache = _getPageStoreCache();
+	page_store_cache[page] = id;
+	_setPageStoreCache(page_store_cache);
+	return id;
+}
+
+function getPageTopElement(page) {
+	var page_store_cache = _getPageStoreCache();
+	var retVal = null;
+	if (page_store_cache) {
+		retVal = page_store_cache[page];
+	}
+	console.log("getPageTopElement(" + page + ") = " + retVal);
+	return retVal;
+}
+
+var scrollTimeout = null;
+var scrollEndDelay = 500; // ms
+
+function findTopVisibleElement() {
+	var found = null;
+	$('.calendar-event').each(function(index, element) {
+		if (elementInViewport(element)) {
+			var id = $(element).attr('id');
+			if (id) {
+				var summary = $(found).find('div.summary').text();
+				console.log("first visible element: " + summary + ' (' + id + ')');
+				if (current_page) {
+					updatePageTopElement(current_page, id);
+				}
+			}
+			found = element;
+			return false;
+		}
+	});
+
+	return found;
+}
+
+function scrollEndHandler() {
+	console.log("scrolling finished");
+	scrollTimeout = null;
+	var found = findTopVisibleElement();
+	if (found) {
+		console.log("visible element: " + $(found).find('div.summary').text() + ' (' + $(found).attr('id') + ')');
+	} else {
+		console.log("no elements visible!");
+	}
+}
+
+$(window).scroll(function() {
+	if (scrollTimeout === null) {
+		console.log("scrolling started");
+	} else {
+		clearTimeout(scrollTimeout);
+	}
+	scrollTimeout = setTimeout( scrollEndHandler, scrollEndDelay );
+});
 
 function onDeviceReady( event ) {
 	console.log("Device is ready.  Initializing.");
@@ -27,21 +125,123 @@ function onDeviceReady( event ) {
     }
 }
 
-var header, container;
+var _header, _container;
+
+function getHeader() {
+	if (!_header) {
+		_header = $("body").find("#header");
+	}
+	return _header;
+}
+
+function getContainer() {
+	if (!_container) {
+		_container = $("body").find("#content");
+	}
+	return _container;
+}
+
+function getScroll() {
+	var scroll;
+	
+	// Netscape compliant
+	if (typeof(window.pageYOffset) === 'number') {
+		scroll = window.pageYOffset;
+	}
+	// DOM compliant
+	else if (document.body && document.body.scrollTop) {
+		scroll = document.body.scrollTop;
+	}
+	// IE6 standards compliant mode
+	else if (document.documentElement && document.documentElement.scrollTop) {
+		scroll = document.documentElement.scrollTop;
+	}
+	// needed for IE6 (when vertical scroll bar is on the top)
+	else {
+		scroll = 0;
+	}
+}
+
+function scrollTo(x, y) {
+	window.scrollTo(x, y);
+}
+
+function setupHeader() {
+    header = getHeader();
+    header.html(templates.header);
+    
+    $('nav').find('a').each(function(index, element) {
+    	var hash = undefined;
+    	if (element.href !== undefined) {
+    		hash = element.href.split('#')[1];
+    	}
+    	if (hash !== undefined && hash != "") {
+    		// $(element).off('click');
+    		$(element).on('click.fndtn touchstart.fndtn', function(e) {
+    			// e.preventDefault();
+            	console.log("navigation event: " + hash);
+            	navigateTo(hash);
+    		});
+    	}
+    });
+
+    $(document).foundationTopBar();
+}
+
+function navigateTo(pageId) {
+	if (pageId == 'official-events') {
+		showOfficialEventsView();
+	} else if (pageId == 'my-events') {
+		showMyEventsView();
+	} else {
+		console.log('unknown page ID: ' + pageId);
+		return false;
+	}
+
+	var topElement = getPageTopElement(pageId);
+	if (topElement) {
+		var matched = null;
+		$('#content').find('.scrollable').each(function(index, element) {
+			var id = $(element).attr('id');
+			if (id == topElement) {
+				matched = element;
+				console.log("matched " + id);
+				return false;
+			} else {
+				console.log("id " + id + " did not match " + topElement);
+			}
+			return true;
+		});
+
+		if (matched) {
+			console.log("scrolling to " + topElement);
+			matched.scrollIntoView(true);
+			window.scrollBy(0, -45);
+		} else {
+			console.log("didn't find an element to scroll to for " + topElement);
+		}
+	} else {
+		console.log("no top element found for " + pageId);
+	}
+
+	return true;
+}
 
 function setupDefaultView() {
     console.log("setting up default view");
 
-    showOfficialEventsView();
+    var current_page = amplify.store('current_page');
+    if (!current_page) {
+    	current_page = 'official-events';
+    	amplify.store('current_page', current_page);
+    }
 
-    $(document).foundationTopBar();
-
-    // initialize model data
-    ko.applyBindings(eventsModel);
+    setupHeader();
+    navigateTo(current_page);
 
     var interval = setInterval(function() {
     	eventsModel.updateDataFromJSON();
-    }, 30000);
+    }, 5000);
 
     // Hide address bar on mobile devices
     /*
@@ -57,13 +257,55 @@ function setupDefaultView() {
 
 }
 
-function showOfficialEventsView() {
-    header = $("body").find("#header");
-    container = $("body").find("#content");
+function replaceCurrentPage(pageId) {
+	getContainer().children().css('display', 'none');
+	var page = $('#' + pageId);
+	page.css('display', 'block');
+    if (!Modernizr.touch) {
+    	// on non-mobile devices, focus the search input
+    	page.find('input[type=search]')[0].focus();
+    }
+    amplify.store('current_page', pageId);
+	return getContainer()[0];
+}
 
-    header.html(templates.header);
-    container.html( Mustache.to_html(templates.events, { event_header: "Official Events" }));
-    container.find('ul.event-list').css('visibility', 'visible');
+function createOfficialEventsView() {
+    if (!pages.official) {
+    	var div = document.createElement('div');
+    	div.setAttribute('id', 'official-events');
+    	$(div).css('display', 'none');
+    	var html = Mustache.to_html(templates.events, { eventType: "official" });
+    	$(div).html(html);
+    	pages.official = div;
+    	var appended = getContainer()[0].appendChild(div);
+        ko.applyBindings(officialEventsModel, appended);
+    }
+}
+
+function showOfficialEventsView() {
+	createOfficialEventsView();
+	var content = replaceCurrentPage('official-events');
+    $(content).find('ul.event-list').css('display', 'block');
+}
+
+function createMyEventsView() {
+    if (!pages.my) {
+    	var div = document.createElement('div');
+    	div.setAttribute('id', 'my-events');
+    	$(div).css('display', 'none');
+    	var html = Mustache.to_html(templates.events, { eventType: "my" });
+    	// console.log("html = " + html);
+    	$(div).html(html);
+    	pages.my = div;
+    	var appended = getContainer()[0].appendChild(div);
+        ko.applyBindings(myEventsModel, appended);
+    }
+}
+
+function showMyEventsView() {
+	createMyEventsView();
+	var content = replaceCurrentPage('my-events');
+    $(content).find('ul.event-list').css('display', 'block');
 }
 
 function onTemplateLoaded(template, key) {
@@ -75,6 +317,10 @@ function onTemplateLoaded(template, key) {
 
 	if ( templates.loaded == templates.requested ) {
 		console.log("all requested templates have been loaded");
+		
+		createOfficialEventsView();
+		createMyEventsView();
+
         setupDefaultView();
     }
 }
