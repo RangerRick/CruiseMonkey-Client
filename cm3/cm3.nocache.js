e6451d44167038e8641d0102a29ae8aff7a4fbe8
Benjamin Reed <rangerrick@befunk.com>
2013-01-21 13:11:32 -0500

    fix some bugs in the 'my' list logic

--- cm3/cm3.nocache.js
+++ cm3/cm3.nocache.js
@@ -1,5 +1,5 @@
 function cm3(){
-  var $intern_0 = '', $intern_36 = '" for "gwt:onLoadErrorFn"', $intern_34 = '" for "gwt:onPropertyErrorFn"', $intern_21 = '"><\/script>', $intern_10 = '#', $intern_82 = '.cache.html', $intern_12 = '/', $intern_24 = '//', $intern_71 = '4D7CF5D84EE21CCCA6660A35E0BA01C7', $intern_73 = '768CAAFA218BC6174B40D591D0F1BD4F', $intern_74 = '92D70E5F3565D85244C965C1B5487294', $intern_75 = '9FA8BD033D15ABC1D3CD37A6914A8F5F', $intern_81 = ':', $intern_72 = ':1', $intern_28 = '::', $intern_95 = '<script defer="defer">cm3.onInjectionDone(\'cm3\')<\/script>', $intern_20 = '<script id="', $intern_93 = '<script language="javascript" src="', $intern_31 = '=', $intern_11 = '?', $intern_33 = 'Bad handler "', $intern_76 = 'CB6D8AFFBBD16C4889927E2A076D74CC', $intern_90 = 'CM3.css', $intern_77 = 'D264AE05DED0CB64923609D23E7D59B8', $intern_91 = 'DOMContentLoaded', $intern_78 = 'E11C9E5A14421D9CBDCE63BEECB13F05', $intern_79 = 'F4F09EA5D7C30C15BD5C66EAB8C13903', $intern_80 = 'F7594FD71C72833961A4FF7993DB6077', $intern_22 = 'SCRIPT', $intern_19 = '__gwt_marker_cm3', $intern_42 = 'android', $intern_23 = 'base', $intern_15 = 'baseUrl', $intern_4 = 'begin', $intern_46 = 'blackberry', $intern_3 = 'bootstrap', $intern_14 = 'clear.cache.gif', $intern_1 = 'cm3', $intern_17 = 'cm3.nocache.js', $intern_27 = 'cm3::', $intern_30 = 'content', $intern_53 = 'datetime', $intern_9 = 'end', $intern_47 = 'file://', $intern_65 = 'gecko', $intern_66 = 'gecko1_8', $intern_5 = 'gwt.codesvr=', $intern_6 = 'gwt.hosted=', $intern_7 = 'gwt.hybrid', $intern_89 = 'gwt/clean/clean.css', $intern_35 = 'gwt:onLoadErrorFn', $intern_32 = 'gwt:onPropertyErrorFn', $intern_29 = 'gwt:property', $intern_88 = 'head', $intern_69 = 'hosted.html?cm3', $intern_87 = 'href', $intern_64 = 'ie6', $intern_63 = 'ie8', $intern_62 = 'ie9', $intern_37 = 'iframe', $intern_13 = 'img', $intern_51 = 'input', $intern_43 = 'ipad', $intern_45 = 'iphone', $intern_44 = 'ipod', $intern_38 = "javascript:''", $intern_84 = 'link', $intern_68 = 'loadExternalRefs', $intern_25 = 'meta', $intern_40 = 'moduleRequested', $intern_8 = 'moduleStartup', $intern_61 = 'msie', $intern_56 = 'n', $intern_26 = 'name', $intern_49 = 'no', $intern_58 = 'opera', $intern_41 = 'phonegap.env', $intern_39 = 'position:absolute;width:0;height:0;border:none', $intern_85 = 'rel', $intern_60 = 'safari', $intern_16 = 'script', $intern_70 = 'selectingPermutation', $intern_2 = 'startup', $intern_86 = 'stylesheet', $intern_50 = 'supportsDateTimeInput', $intern_54 = 'text', $intern_83 = 'timebox.css', $intern_52 = 'type', $intern_18 = 'undefined', $intern_67 = 'unknown', $intern_57 = 'user.agent', $intern_92 = 'uuid.js', $intern_94 = 'uuid.js"><\/script>', $intern_59 = 'webkit', $intern_55 = 'y', $intern_48 = 'yes';
+  var $intern_0 = '', $intern_36 = '" for "gwt:onLoadErrorFn"', $intern_34 = '" for "gwt:onPropertyErrorFn"', $intern_21 = '"><\/script>', $intern_10 = '#', $intern_82 = '.cache.html', $intern_12 = '/', $intern_24 = '//', $intern_71 = '17AC9F8CB8A8726A1BFFBABAAFC439EE', $intern_72 = '27634A1C81677ABBDDD27F2C86D7B7E4', $intern_74 = '878E4DDEA39B9801091B28CA402F0329', $intern_75 = '9A5D5A4E5D313222F3A7B32C9DEE2C96', $intern_81 = ':', $intern_73 = ':1', $intern_28 = '::', $intern_95 = '<script defer="defer">cm3.onInjectionDone(\'cm3\')<\/script>', $intern_20 = '<script id="', $intern_93 = '<script language="javascript" src="', $intern_31 = '=', $intern_11 = '?', $intern_76 = 'A724C0CAFE0136B3135B296B9A5D3EDB', $intern_33 = 'Bad handler "', $intern_77 = 'C170F0DFB230E72C055DDE6A15988E92', $intern_90 = 'CM3.css', $intern_91 = 'DOMContentLoaded', $intern_78 = 'E2AE66A37C8E472541C37FC167AEFDD0', $intern_79 = 'EF4BFEEBE5B5F945A20F01F4349C8D62', $intern_80 = 'F86EBD1241EC336F82D0DE175B010019', $intern_22 = 'SCRIPT', $intern_19 = '__gwt_marker_cm3', $intern_42 = 'android', $intern_23 = 'base', $intern_15 = 'baseUrl', $intern_4 = 'begin', $intern_46 = 'blackberry', $intern_3 = 'bootstrap', $intern_14 = 'clear.cache.gif', $intern_1 = 'cm3', $intern_17 = 'cm3.nocache.js', $intern_27 = 'cm3::', $intern_30 = 'content', $intern_53 = 'datetime', $intern_9 = 'end', $intern_47 = 'file://', $intern_65 = 'gecko', $intern_66 = 'gecko1_8', $intern_5 = 'gwt.codesvr=', $intern_6 = 'gwt.hosted=', $intern_7 = 'gwt.hybrid', $intern_89 = 'gwt/clean/clean.css', $intern_35 = 'gwt:onLoadErrorFn', $intern_32 = 'gwt:onPropertyErrorFn', $intern_29 = 'gwt:property', $intern_88 = 'head', $intern_69 = 'hosted.html?cm3', $intern_87 = 'href', $intern_64 = 'ie6', $intern_63 = 'ie8', $intern_62 = 'ie9', $intern_37 = 'iframe', $intern_13 = 'img', $intern_51 = 'input', $intern_43 = 'ipad', $intern_45 = 'iphone', $intern_44 = 'ipod', $intern_38 = "javascript:''", $intern_84 = 'link', $intern_68 = 'loadExternalRefs', $intern_25 = 'meta', $intern_40 = 'moduleRequested', $intern_8 = 'moduleStartup', $intern_61 = 'msie', $intern_56 = 'n', $intern_26 = 'name', $intern_49 = 'no', $intern_58 = 'opera', $intern_41 = 'phonegap.env', $intern_39 = 'position:absolute;width:0;height:0;border:none', $intern_85 = 'rel', $intern_60 = 'safari', $intern_16 = 'script', $intern_70 = 'selectingPermutation', $intern_2 = 'startup', $intern_86 = 'stylesheet', $intern_50 = 'supportsDateTimeInput', $intern_54 = 'text', $intern_83 = 'timebox.css', $intern_52 = 'type', $intern_18 = 'undefined', $intern_67 = 'unknown', $intern_57 = 'user.agent', $intern_92 = 'uuid.js', $intern_94 = 'uuid.js"><\/script>', $intern_59 = 'webkit', $intern_55 = 'y', $intern_48 = 'yes';
   var $wnd = window, $doc = document, $stats = $wnd.__gwtStatsEvent?function(a){
     return $wnd.__gwtStatsEvent(a);
   }
@@ -326,30 +326,30 @@ function cm3(){
   $stats && $stats({moduleName:$intern_1, sessionId:$sessionId, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date).getTime(), type:$intern_70});
   if (!isHostedMode()) {
     try {
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_58], $intern_71);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_58], $intern_71);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_58], $intern_71 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_58], $intern_71 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_48, $intern_55, $intern_60], $intern_73);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_64], $intern_74);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_64], $intern_74);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_64], $intern_74 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_64], $intern_74 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_60], $intern_75);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_63], $intern_76);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_63], $intern_76);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_63], $intern_76 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_63], $intern_76 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_60], $intern_77);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_62], $intern_78);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_62], $intern_78);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_62], $intern_78 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_62], $intern_78 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_48, $intern_56, $intern_60], $intern_79);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_66], $intern_80);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_66], $intern_80);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_66], $intern_80 + $intern_72);
-      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_66], $intern_80 + $intern_72);
+      unflattenKeylistIntoAnswers([$intern_48, $intern_55, $intern_60], $intern_71);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_66], $intern_72);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_66], $intern_72);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_66], $intern_72 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_66], $intern_72 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_60], $intern_74);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_62], $intern_75);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_62], $intern_75);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_62], $intern_75 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_62], $intern_75 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_48, $intern_56, $intern_60], $intern_76);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_63], $intern_77);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_63], $intern_77);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_63], $intern_77 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_63], $intern_77 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_58], $intern_78);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_58], $intern_78);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_58], $intern_78 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_58], $intern_78 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_60], $intern_79);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_64], $intern_80);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_64], $intern_80);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_56, $intern_64], $intern_80 + $intern_73);
+      unflattenKeylistIntoAnswers([$intern_49, $intern_55, $intern_64], $intern_80 + $intern_73);
       strongName = answers[computePropValue($intern_41)][computePropValue($intern_50)][computePropValue($intern_57)];
       var idx = strongName.indexOf($intern_81);
       if (idx != -1) {
