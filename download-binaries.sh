b33530b5188e011a1e112c5823d2bd539d306849
Benjamin Reed <rangerrick@befunk.com>
2013-01-23 20:27:29 -0500

    add a script to download the finished files

---
+++ download-binaries.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -L -o CruiseMonkey.ipa "https://build.phonegap.com/apps/211512/download/ios"
+curl -L -o CruiseMonkey.xap "https://build.phonegap.com/apps/211512/download/winphone"
+curl -L -o CruiseMonkey.ipk "https://build.phonegap.com/apps/211512/download/webos"
+curl -L -o CruiseMonkey.apk "https://build.phonegap.com/apps/211512/download/android"
+curl -L -o CruiseMonkey.wgz "https://build.phonegap.com/apps/211512/download/symbian"
